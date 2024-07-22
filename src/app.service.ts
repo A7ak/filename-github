84fd9f728ce18dcdcd986819c6c819467deacbcc
a7 <ag7aka@gmail.com>
2024-07-23 01:30:17 +0530

    implemented github api

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import { log } from 'console';
+import { response } from 'express';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly httpService: HttpService) {}
+
+  getGithubRepo() : Observable<any>  {
+
+    const url = 'https://api.github.com/repos/A7ak/filename-github/contents/src/app.service.ts';
+    const token = `token`
+    const headers = {
+      Authorization: token,
+      Accept: 'application/vnd.github.v3.raw',
+    };
+    console.log("data")
+    return this.httpService.get(url,{ headers}).pipe(
+      map((response: AxiosResponse) => response.data),
+    );
+  }
+
   getHello(): string {
     return 'First nestjs project!';
   }
