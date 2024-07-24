2d6d3883e9c9cd4e357ec1527cc216914bb24eab
A7ak <51486750+A7ak@users.noreply.github.com>
2024-07-25 02:28:34 +0530

    improvent in test cases

--- src/test.kt
+++ src/test.kt
@@ -1,9 +1,11 @@
 
+// for java code
 textView1.setText("api.response.key1");
 
 textView2.setText(
     api.response.key2);
 
+// for kotlin code 
 textView3.text = api.response.key3   
 
 textView4.text = 
